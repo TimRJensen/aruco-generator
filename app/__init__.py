3b23d83052ad8ff66ee38f0d1866bf42d8c73b4c
Tim R Jensen <timrjensen@gmail.com>
2024-09-21 18:41:18 +0200

    FEAT: Added sanity checks

--- app/__init__.py
+++ app/__init__.py
@@ -7,14 +7,14 @@ from datetime import datetime as dt
 
 # Order matters
 aruco_dicts = [
-    aruco.getPredefinedDictionary(aruco.DICT_4X4_1000),
-    aruco.getPredefinedDictionary(aruco.DICT_5X5_1000),
-    aruco.getPredefinedDictionary(aruco.DICT_6X6_1000),
-    aruco.getPredefinedDictionary(aruco.DICT_7X7_1000),
-    aruco.getPredefinedDictionary(aruco.DICT_APRILTAG_16h5),
-    aruco.getPredefinedDictionary(aruco.DICT_APRILTAG_25h9),
-    aruco.getPredefinedDictionary(aruco.DICT_APRILTAG_36h10),
-    aruco.getPredefinedDictionary(aruco.DICT_APRILTAG_36h11),
+    (aruco.getPredefinedDictionary(aruco.DICT_4X4_1000), 1000),
+    (aruco.getPredefinedDictionary(aruco.DICT_5X5_1000), 1000),
+    (aruco.getPredefinedDictionary(aruco.DICT_6X6_1000), 1000),
+    (aruco.getPredefinedDictionary(aruco.DICT_7X7_1000), 1000),
+    (aruco.getPredefinedDictionary(aruco.DICT_APRILTAG_16h5), 30),
+    (aruco.getPredefinedDictionary(aruco.DICT_APRILTAG_25h9), 35),
+    (aruco.getPredefinedDictionary(aruco.DICT_APRILTAG_36h10), 2320),
+    (aruco.getPredefinedDictionary(aruco.DICT_APRILTAG_36h11), 587),
 ]
 
 def clean_temp(path):
@@ -34,13 +34,16 @@ def create_app():
         n = request.args.get("dict", type=int)
         id = request.args.get("id", type=int)
         size = request.args.get("size", type=int)
+
+        if id >= aruco_dicts[n][1]:
+            return send_file("static/img/404.png", mimetype="image/png")
+        
         path = os.path.abspath("temp/marker-{0}_{1}.png".format(
             id, 
             dt.now().strftime("%d-%m-%yT%H-%M-%S"))
         )
-        print(size)
-        cv2.imwrite(path, aruco.drawMarker(aruco_dicts[n], id, size))
+        cv2.imwrite(path, aruco.drawMarker(aruco_dicts[n][0], id, size))
         Timer(5, clean_temp, args=[path]).start()
-        return send_file(path, mimetype="image/png", etag=True)
+        return send_file(path, mimetype="image/png")
     
     return app
